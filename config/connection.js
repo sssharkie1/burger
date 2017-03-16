fa99691d2dd81ea6bd2880535b6c99bb01b3147a
Sarah Stevens <sssharkie1>
2017-03-16 18:16:54 -0400

    added JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "sstevens",
-  password: "Larryland102!",
-  database: "burger_db"
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWS_URL);
+  } else {
+
+    connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "sstevens",
+    password: "Larryland102!",
+    database: "burger_db"
+});
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
